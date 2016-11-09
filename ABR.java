255b885c5eb3c6f5b847f789f3a5feefc27489cb
jeckerm <marie.jecker@depinfonancy.net>
2016-11-09 14:35:39 +0100

    Add files via upload

--- ABR.java
+++ ABR.java
@@ -1,16 +1,31 @@
 
 public class ABR {
 
+	private int elements;
+	private Node root;
+	
 	public ABR(){
-		
+		this.elements = 0;
+		this.root = null;
 	}
 	
-	public void insert(int i){
-		
+	public void insert(char i){
+		Node n = new Node(i);
+		if (this.root == null){
+			this.root = n;
+		}
+		elements += 1;
 	}
 	
 	public boolean isEmpty(){
+		if (this.elements == 0){
+			return true;
+		}
 		return false;
+	}
+	
+	public int nbElements(){
+		return this.elements;
 	}
 	
 }
