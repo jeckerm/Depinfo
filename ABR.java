defa68f179af396735aa7dbc854645a24c0c2471
jeckerm <marie.jecker@depinfonancy.net>
2016-11-09 15:38:55 +0100

    Add files via upload

--- ABR.java
+++ ABR.java
@@ -2,37 +2,44 @@
 public class ABR {
 
 	private int elements;
-	private Node root;
+	public Node root;
 	
 	public ABR(){
 		this.elements = 0;
 		this.root = null;
 	}
 	
-	public void insert(char i){
-		Node n = new Node(i);
-		if (this.root == null){
-			this.root = n;
-		} else {
-			Node next = this.root;
-			Node m = null;
-			while (next != null){ 	//On compare le node aux nodes déjà existants
-				m = next;			// pour le placer au bon endroit dans l'arbre
-				if (n.getValeur() < next.getValeur()){
-					next = next.child_left;
+	public void insert(int i){
+		if (this.contains(i)){
+			System.out.println("Cet élément est déjà dans l'arbre");
+		} else { 
+			Node n = new Node(i);
+			if (this.root == null){
+				this.root = n;
+			} else {
+				Node next = this.root;	//On initialise la variable à la racine
+				Node m = next;			//On aura besoin de se rappeler de la variable précédant chaque test
+				while (next != null){ 	//On compare le node aux nodes déjà existants
+					m = next;			//pour le placer au bon endroit dans l'arbre
+					if (i < next.getValeur()){
+						next = next.child_left;
+						System.out.println("ok tout va bien");
+					} else {
+						next = next.child_right;
+					}
+				}
+				//on s'est arrêté au dernier node correspondant, on veut maintenant savoir
+				//si on le place à gauche ou à droite
+				if (i < m.getValeur()){
+					n = m.child_left;
+					System.out.println("L'élément est l'enfant de gauche du noeud");
 				} else {
-					next = next.child_right;
+					n = m.child_right;
+					System.out.println("L'élément est l'enfant de droite du noeud");
 				}
 			}
-			//on s'est arrêté au dernier node correspondant, on veut maintenant savoir
-			//si on le place à gauche ou à droite
-			if (n.getValeur() < m.getValeur()){
-				n = m.child_left;
-			} else {
-				n = m.child_right;
-			}
+			elements += 1;
 		}
-		elements += 1;
 	}
 	
 	public boolean isEmpty(){
@@ -44,6 +51,20 @@ public class ABR {
 	
 	public int nbElements(){
 		return this.elements;
+	}
+	
+	public boolean contains(int i){
+		Node next = root;
+		while (next != null){
+			if (i < next.getValeur()){
+				next = next.child_left;
+			} if (i > next.getValeur()){
+				next = next.child_right;
+			} else {
+				return(true);
+			}
+		}
+		return false;
 	}
 	
 }
--- Node.java
+++ Node.java
@@ -2,15 +2,15 @@ public class Node {
 
 	public Node child_left;
 	public Node child_right;
-	private char valeur;
+	private int valeur;
 	
-	public Node(char i){
+	public Node(int i){
 		this.child_left = null;
 		this.child_right = null;
 		this.valeur = i;
 	}
 	
-	public char getValeur(){
+	public int getValeur(){
 		return this.valeur;
 	}
 
